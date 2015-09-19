5aeb96ce074910ba010caee4744149d49972f69c
kerrygrover <prateekgrover.rns@gmail.com>
2015-09-19 15:58:44 +0530

    Adds project

---
+++ ChatTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatTextView
+//
+//  Created by Prateek Grover on 06/08/15.
+//  Copyright (c) 2015 Prateek Grover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatTextView
+//
+//  Created by Prateek Grover on 06/08/15.
+//  Copyright (c) 2015 Prateek Grover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
